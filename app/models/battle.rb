e56e5d63bc6c9477bb36c7fe298948f58004922a
annapolec <ania.polec@gmail.com>
2016-07-11 17:06:25 +0200

    Add association between challenge and battle

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,5 +1,7 @@
 class Battle < ActiveRecord::Base
   has_many :battle_members
+  has_many :challenges
+  
   enum mode: [ :for_users, :for_group ]
 
   def persisted_members
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,6 @@
 class Challenge < ActiveRecord::Base
+  belongs_to :battle
+  
   validates :name, :points, presence: true
   validates :points, numericality: { greater_than: 0 }
 end
