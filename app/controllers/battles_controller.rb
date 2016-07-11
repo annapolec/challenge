6e734f7b6196fa67b8dc60938b80c09d0c75f69c
annapolec <ania.polec@gmail.com>
2016-07-11 15:26:57 +0200

    Add controller and routes for battles

---
+++ app/controllers/battles_controller.rb
@@ -0,0 +1,58 @@
+class BattlesController < ApplicationController
+  def create
+    @battle = Battle.new(battle_params)
+
+    if @battle.save
+      respond_to do |format|
+        format.html { redirect_to @battle }
+        format.json { render json: @battle }
+      end
+    end    
+  end
+
+  def index
+    @battles = Battle.all
+    @battle = Battle.new
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @battles }
+    end
+  end
+
+  def show
+    @battle = Battle.find(params[:id])
+  end
+
+  def edit
+    @battle = Battle.find(params[:id])
+  end
+
+  def update
+    @battle = Battle.find(params[:id])
+
+    if @battle.update_attributes(battle_params)
+      respond_to do |format|
+        format.html { redirect_to @battle }
+        format.json { render json: @battle }
+      end
+    end
+  end
+
+  def destroy
+    @battle = Battle.find(params[:id])
+    
+    if @battle.destroy
+      respond_to do |format|
+        format.html { redirect_to battles_path }
+        format.json { render json: { stauts: 200 }.to_json }
+      end
+    end
+  end
+
+  private
+
+  def battle_params
+    params.require(:battle).permit(:name, :owner_id, :mode)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :battles
+
+  authenticated :user do
+    root :to => "battles#index", as: :authenticated_root
+  end
   devise_scope :user do
     root :to => "devise/sessions#new"
   end
-end
+ end
