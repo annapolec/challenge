d24b80b6bd974c6654c63223851994e1498a1b6a
annapolec <ania.polec@gmail.com>
2016-07-11 22:11:08 +0200

    Not allow assigning same user to a group twice

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,4 +7,8 @@ class Group < ActiveRecord::Base
   has_many :challenges, through: :challenge_members
   
   validates :name, presence: true
+
+  def not_assigned_users
+    User.where.not(id: self.group_users.pluck(:user_id))
+  end
 end
