295a0031f7409af096cd338f4f702209892e368a
annapolec <ania.polec@gmail.com>
2016-07-11 17:39:58 +0200

    Add model and migration for group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711145948) do
+ActiveRecord::Schema.define(version: 20160711153728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 20160711145948) do
     t.datetime "updated_at", null: false
     t.index ["battle_id"], name: "index_challenges_on_battle_id", using: :btree
     t.index ["owner_id"], name: "index_challenges_on_owner_id", using: :btree
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
