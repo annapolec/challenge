f346fbf860f91f64aeaba25480babe0d6885a5f6
annapolec <ania.polec@gmail.com>
2016-07-11 15:52:35 +0200

    Add associations between user, battle and battle_member

--- app/models/battle_member.rb
+++ app/models/battle_member.rb
@@ -1,3 +1,6 @@
 class BattleMember < ActiveRecord::Base
+  belongs_to :member, polymorphic: true
+  belongs_to :battle
+  
   validates :battle_id, :member_id, :member_type, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :battle_members, as: :member
+  has_many :battles, through: :battle_members
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
